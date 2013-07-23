4be586157d513694ede25550572b028380489e6f
Francis Russell <francis@unchartedbackwaters.co.uk>
2013-07-23 01:16:35 +0100

    Add tests for split format interpolation.

--- bin/benchmark.c
+++ bin/benchmark.c
@@ -9,6 +9,135 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
+
+typedef enum
+{
+  INTERLEAVED,
+  SPLIT
+} storage_layout_t;
+
+typedef struct
+{
+  storage_layout_t layout;
+  union
+  {
+    fftw_complex *interleaved;
+    struct
+    {
+      double *real;
+      double *imag;
+    } split;
+  } coarse;
+  union
+  {
+    fftw_complex *interleaved;
+    struct
+    {
+      double *real;
+      double *imag;
+    } split;
+  } fine;
+} storage_t;
+
+static void allocate_storage(storage_t *storage, storage_layout_t layout, int size)
+{
+  assert(storage != NULL);
+  storage->layout = layout;
+
+  switch(layout)
+  {
+    case INTERLEAVED:
+      storage->coarse.interleaved = rs_alloc_complex(size);
+      storage->fine.interleaved = rs_alloc_complex(8 * size);
+      break;
+    case SPLIT:
+      storage->coarse.split.real = rs_alloc_real(size);
+      storage->coarse.split.imag = rs_alloc_real(size);
+      storage->fine.split.real = rs_alloc_real(8 * size);
+      storage->fine.split.imag = rs_alloc_real(8 * size);
+      break;
+    default:
+      fprintf(stderr, "Unknown storage type %d\n", layout);
+      exit(EXIT_FAILURE);
+  }
+}
+
+static void free_storage(storage_t *storage)
+{
+  assert(storage != NULL);
+
+  switch(storage->layout)
+  {
+    case INTERLEAVED:
+      rs_free(storage->coarse.interleaved);
+      rs_free(storage->fine.interleaved);
+      break;
+    case SPLIT:
+      rs_free(storage->coarse.split.real);
+      rs_free(storage->coarse.split.imag);
+      rs_free(storage->fine.split.real);
+      rs_free(storage->fine.split.imag);
+      break;
+    default:
+      fprintf(stderr, "Unknown storage type %d\n", storage->layout);
+      exit(EXIT_FAILURE);
+  }
+}
+
+static void set_element(storage_t *storage, int offset, fftw_complex value)
+{
+  assert(storage != NULL);
+
+  switch(storage->layout)
+  {
+    case INTERLEAVED:
+      storage->coarse.interleaved[offset] = value;
+      break;
+    case SPLIT:
+      storage->coarse.split.real[offset] = creal(value);
+      storage->coarse.split.imag[offset] = cimag(value);
+      break;
+    default:
+      fprintf(stderr, "Unknown storage type %d\n", storage->layout);
+      exit(EXIT_FAILURE);
+  }
+}
+
+static void execute_interpolate(storage_t *storage, interpolate_plan plan)
+{
+  assert(storage != NULL);
+
+  switch(storage->layout)
+  {
+    case INTERLEAVED:
+      interpolate_execute_interleaved(plan, storage->coarse.interleaved, storage->fine.interleaved);
+      break;
+    case SPLIT:
+      interpolate_execute_split(plan, storage->coarse.split.real, storage->coarse.split.imag, storage->fine.split.real, storage->fine.split.imag);
+      break;
+    default:
+      fprintf(stderr, "Unknown storage type %d\n", storage->layout);
+      exit(EXIT_FAILURE);
+  }
+}
+
+static fftw_complex get_element(storage_t *storage, int offset)
+{
+  assert(storage != NULL);
+
+  switch(storage->layout)
+  {
+    case INTERLEAVED:
+      return storage->fine.interleaved[offset];
+    case SPLIT:
+      return storage->fine.split.real[offset] + storage->fine.split.imag[offset] * I;
+      break;
+    default:
+      fprintf(stderr, "Unknown storage type %d\n", storage->layout);
+      exit(EXIT_FAILURE);
+  }
+}
 
 typedef interpolate_plan (*plan_constructor_t)(int n0, int n1, int n2, int flags);
 
@@ -20,53 +149,50 @@ static double test_function(double x, double y, double z)
 }
 
 static void perform_timing(plan_constructor_t constructor,
-  const int x_width, const int y_width, const int z_width)
+  storage_t *storage, const int z_width, const int y_width, const int x_width)
 {
   time_point_t begin_resample, end_resample, begin_plan, end_plan;
 
-  fftw_complex *in = rs_alloc_complex(x_width * y_width * z_width);
-  fftw_complex *out = rs_alloc_complex(8 * x_width * y_width * z_width);
-
   time_point_save(&begin_plan);
-  interpolate_plan plan = constructor(x_width, y_width, z_width, 0);
+  interpolate_plan plan = constructor(z_width, y_width, x_width, 0);
   time_point_save(&end_plan);
 
 
-  for(int x=0; x < x_width; ++x)
+  for(int z=0; z < z_width; ++z)
   {
     for(int y=0; y < y_width; ++y)
     {
-      for(int z=0; z < z_width; ++z)
+      for(int x=0; x < x_width; ++x)
       {
-        const int offset = x*y_width*z_width + y*z_width + z;
+        const int offset = z*y_width*x_width + y*x_width + x;
         const double x_pos = (x * 2.0 * pi)/x_width;
         const double y_pos = (y * 2.0 * pi)/y_width;
         const double z_pos = (z * 2.0 * pi)/z_width;
 
-        in[offset] = test_function(x_pos, y_pos, z_pos);
+        set_element(storage, offset, test_function(x_pos, y_pos, z_pos));
       }
     }
   }
 
   time_point_save(&begin_resample);
-  interpolate_execute_interleaved(plan, in, out);
+  execute_interpolate(storage, plan);
   time_point_save(&end_resample);
 
   double abs_val = 0.0;
 
-  for(int x=0; x < 2 * x_width; ++x)
+  for(int z=0; z < 2 * z_width; ++z)
   {
     for(int y=0; y < 2 * y_width; ++y)
     {
-      for(int z=0; z < 2 * z_width; ++z)
+      for(int x=0; x < 2 * z_width; ++x)
       {
-        const int offset = 4*x*y_width*z_width + 2*y*z_width + z;
+        const int offset = 4*z*y_width*x_width + 2*y*x_width + x;
         const double x_pos = (x * pi)/x_width;
         const double y_pos = (y * pi)/y_width;
         const double z_pos = (z * pi)/z_width;
 
         const double expected = test_function(x_pos, y_pos, z_pos);
-        abs_val += cabs(out[offset] - expected);
+        abs_val += cabs(get_element(storage, offset) - expected);
       }
     }
   }
@@ -78,17 +204,30 @@ static void perform_timing(plan_constructor_t constructor,
   printf("Execution time: %f\n", time_point_delta(&begin_resample, &end_resample));
   printf("Delta: %f\n", abs_val);
 
-  rs_free(in);
-  rs_free(out);
   interpolate_destroy_plan(plan);
 }
 
 int main(int argc, char **argv)
 {
-  perform_timing(interpolate_plan_3d_naive_interleaved, 75, 75, 75);
+  int z = 75, y = 75, x = 75;
+  storage_t storage;
+
+  allocate_storage(&storage, INTERLEAVED, x * y * z);
+  perform_timing(interpolate_plan_3d_naive_interleaved, &storage, z, y, x);
   printf("\n");
-  perform_timing(interpolate_plan_3d_padding_aware_interleaved, 75, 75, 75);
+  perform_timing(interpolate_plan_3d_padding_aware_interleaved, &storage, z, y, x);
   printf("\n");
-  perform_timing(interpolate_plan_3d_phase_shift_interleaved, 75, 75, 75);
+  perform_timing(interpolate_plan_3d_phase_shift_interleaved, &storage, z, y, x);
+  free_storage(&storage);
+
+  printf("\n\n");
+  allocate_storage(&storage, SPLIT, x * y * z);
+  perform_timing(interpolate_plan_3d_naive_split, &storage, z, y, x);
+  printf("\n");
+  perform_timing(interpolate_plan_3d_padding_aware_split, &storage, z, y, x);
+  printf("\n");
+  perform_timing(interpolate_plan_3d_phase_shift_split, &storage, z, y, x);
+  free_storage(&storage);
+
   return EXIT_SUCCESS;
 }
