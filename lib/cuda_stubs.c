6132ee9b48e70d16076accadf4b8097953d9ce5b
Francis Russell <francis@unchartedbackwaters.co.uk>
2013-11-13 15:26:13 +0000

    Add stubs for when nvcc is not available.

---
+++ lib/cuda_stubs.c
@@ -0,0 +1,35 @@
+#include "naive_cuda.h"
+#include "padding_aware_cuda.h"
+#include <stddef.h>
+
+interpolate_plan interpolate_plan_3d_naive_cuda_interleaved(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+interpolate_plan interpolate_plan_3d_naive_cuda_split(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+interpolate_plan interpolate_plan_3d_naive_cuda_product(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+interpolate_plan interpolate_plan_3d_padding_aware_cuda_interleaved(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+interpolate_plan interpolate_plan_3d_padding_aware_cuda_split(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+interpolate_plan interpolate_plan_3d_padding_aware_cuda_product(int n0, int n1, int n2, int flags)
+{
+  return NULL;
+}
+
+
